fe4224b00dcdeec1c34a6c2941d3f6b1fcd44dbd
PauloTuppy <paulotuppy5@gmail.com>
2025-09-09 04:44:46 +0000

    Update Tailwind and PostCSS configs to use module.exports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}', './**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
+module.exports = {
+  content: [
+    "./index.html",
+    "./**/*.tsx",
+    "./**/*.ts",
+    "./**/*.jsx",
+    "./**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
